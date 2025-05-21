9b98d5d60d17d79e0066fcbd31cbd7d16fe0a44d
Pham Tuan <tuan.pham.super@gmail.com>
2025-05-21 11:38:54 +0700

    fix cors policy

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5174','http://localhost:5173',"http://127.0.0.1:8000"],
+    'allowed_origins' => ['http://localhost:5174','http://localhost:5173',"http://127.0.0.1:8000","https://fla-dev-lms.vercel.app"],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'X-CSRF-TOKEN'],
     'exposed_headers' => [],
