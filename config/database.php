308e7a250dbf2a9d2cded8a1f28fb550150e4f86
Pham Tuan <tuan.pham.super@gmail.com>
2025-05-21 14:39:54 +0700

    too tired

--- config/database.php
+++ config/database.php
@@ -115,8 +115,8 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('MONGODB_URI'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '27017'),
+            'host' => env('MONGODB_URI') ? null : env('DB_HOST', '127.0.0.1'),
+            'port' => env('MONGODB_URI') ? null : env('DB_PORT', '27017'),
             'database' => env('DATABASE_NAME', 'lms-backend'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
